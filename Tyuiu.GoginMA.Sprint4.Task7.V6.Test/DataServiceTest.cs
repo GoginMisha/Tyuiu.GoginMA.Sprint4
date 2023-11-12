13503f3cdeb6eb3ddbaec4be80d79c2a6ce9149c
Tyuiu.GoginMA@outlook.com <partycat53241@gmail.com>
2023-11-12 20:20:57 +0500

    Task7

---
+++ Tyuiu.GoginMA.Sprint4.Task7.V6.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.GoginMA.Sprint4.Task7.V6.Lib;
+
+namespace Tyuiu.GoginMA.Sprint4.Task7.V6.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void Calculate()
+        {
+            DataService ds = new DataService();
+            int rows = 3;
+            int columns = 4;
+            int[,] mtrx = new int[rows, columns];
+            string str = "458963214789";
+            int res = ds.Calculate(rows, columns, str);
+            int wait = 34;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.GoginMA.Sprint4.Task7.V6.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.GoginMA.Sprint4.Task7.V6.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.GoginMA.Sprint4.Task7.V6.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1b7d9657-56fa-40ae-bca6-500386ab9805")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
