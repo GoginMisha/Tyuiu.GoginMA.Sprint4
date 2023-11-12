bbc1be6584f5e4e9909806a01e509aef1fc7b551
Tyuiu.GoginMA@outlook.com <partycat53241@gmail.com>
2023-11-12 11:51:05 +0500

    Task5

---
+++ Tyuiu.GoginMA.Sprint4.Task5.V28.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.GoginMA.Sprint4.Task5.V28.Lib;
+
+namespace Tyuiu.GoginMA.Sprint4.Task5.V28.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void Calculate()
+        {
+            DataService ds = new DataService();
+            int[,] mas2 = new int[5, 5] { { -6, -5, -4, -3, -2 }, { -1, 0, 1, 2, 3 }, { -6, -5, -4, -3, -2 }, { -1, 0, 1, 2, 3 }, { -6, -5, -4, -3, -2 } };
+            int[,] res = ds.Calculate(mas2);
+            int[,] wait = { { 0, 0, 0, 0, 0 }, { 0, 0, 1, 2, 3 }, { 0, 0, 0, 0, 0 }, { 0, 0, 1, 2, 3 }, { 0, 0, 0, 0, 0 } };
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.GoginMA.Sprint4.Task5.V28.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.GoginMA.Sprint4.Task5.V28.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.GoginMA.Sprint4.Task5.V28.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3f60c448-9986-46d8-8baa-2f325345bc55")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
